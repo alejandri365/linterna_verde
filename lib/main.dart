9168b24991cd0242bd2ec4498faf5a16851d878b
alejandri365 <ventasalejandrocedres@gmail.com>
2025-11-28 14:27:46 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
-// Simulación del archivo main.dart para Codemagic
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(LinternaVerdeApp());
+
+class LinternaVerdeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Linterna Verde',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Linterna Verde')),
+        body: Center(child: Text('Bienvenido a Linterna Verde')),
+      ),
+    );
+  }
+}
